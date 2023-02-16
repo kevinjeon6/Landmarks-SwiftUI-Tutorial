769f17a132477e4fff98b93245556e7a9be05c38
Kevin <kevin_jeon@icloud.com>
2023-02-15 23:47:01 -0500

    Create new view with name of CircleImage Add turtlerock image

---
+++ Apple-SwiftUI-Tutorial/CircleImageView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImageView.swift
+//  Apple-SwiftUI-Tutorial
+//
+//  Created by Kevin Mattocks on 2/15/23.
+//
+
+import SwiftUI
+
+struct CircleImageView: View {
+    var body: some View {
+        Image("turtlerock")
+    }
+}
+
+struct CircleImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImageView()
+    }
+}
