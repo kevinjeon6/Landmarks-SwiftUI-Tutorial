3d4e3b71703fc2de78d16295c6fb9a159fe5247d
Kevin <kevin_jeon@icloud.com>
2023-02-15 23:24:44 -0500

    Initial Commit

---
+++ Apple-SwiftUI-Tutorial/Apple_SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_SwiftUI_TutorialApp.swift
+//  Apple-SwiftUI-Tutorial
+//
+//  Created by Kevin Mattocks on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
