cff21b1774e5d2f48cd57b902719c63f8ef57067
Kevin <kevin_jeon@icloud.com>
2023-02-15 23:33:33 -0500

    Start the Landmark app that is Apple SwiftUI tutorial to get more practice and continue learning

--- Apple-SwiftUI-Tutorial/ContentView.swift
+++ Apple-SwiftUI-Tutorial/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            Text("Joshua Tree National Park")
+                .font(.headline)
         }
-        .padding()
+        
     }
 }
 
