54e8241511d7822c7d5ebf8050b294438e272208
Kevin <kevin_jeon@icloud.com>
2023-02-17 00:19:51 -0500

    Add MapView and CircleImageView to ContentView The offset is -130 vertically.

--- Apple-SwiftUI-Tutorial/ContentView.swift
+++ Apple-SwiftUI-Tutorial/ContentView.swift
@@ -9,17 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.headline)
-                Spacer()
-                Text("California")
+        VStack {
+            MapView()
+                .frame(height: 300)
+            CircleImageView()
+                .offset(y: -130) //Offset is based from the bottom of the screen. More negative, the higher the image
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.headline)
+                    Spacer()
+                    Text("California")
+                }
             }
+            .padding()
         }
-        .padding()
         
     }
 }
