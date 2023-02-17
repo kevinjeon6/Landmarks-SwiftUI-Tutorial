85c03d4c0df66bdb03cb9a8ee3bed15b25be23ae
Kevin <kevin_jeon@icloud.com>
2023-02-16 23:33:40 -0500

    Clip shape image as a circle and add a white border with a shadow

--- Apple-SwiftUI-Tutorial/CircleImageView.swift
+++ Apple-SwiftUI-Tutorial/CircleImageView.swift
@@ -10,6 +10,12 @@ import SwiftUI
 struct CircleImageView: View {
     var body: some View {
         Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle()
+                    .stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
     }
 }
 
