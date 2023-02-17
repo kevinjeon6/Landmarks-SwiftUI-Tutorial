3a4b38dcde2aef74e265f0d41952af3e5e135b7c
Kevin <kevin_jeon@icloud.com>
2023-02-17 14:40:11 -0500

    Add properties to the Landmarks model

--- Apple-SwiftUI-Tutorial/Model/LandMark.swift
+++ Apple-SwiftUI-Tutorial/Model/LandMark.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct Landmark: Hashable, Codable {
+    let id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+}
