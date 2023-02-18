b117167b9ed0e705662708fab0feb4af33fa1b66
Kevin <kevin_jeon@icloud.com>
2023-02-17 21:07:21 -0500

    Create variable for imageName and create computed image property to upload the image from JSON
    Create a nested Coordinates struct with properties relating to JSON data format
    
    Create coordinates variable and computed coordinates property

--- Apple-SwiftUI-Tutorial/Model/LandMark.swift
+++ Apple-SwiftUI-Tutorial/Model/LandMark.swift
@@ -5,7 +5,9 @@
 //  Created by Kevin Mattocks on 2/17/23.
 //
 
+import CoreLocation
 import Foundation
+import SwiftUI
 
 struct Landmark: Hashable, Codable {
     let id: Int
@@ -13,5 +15,30 @@ struct Landmark: Hashable, Codable {
     var park: String
     var state: String
     var description: String
+    
+    
+    private var imageName: String
+    
+    //Computed image property to upload the image from the JSON data
+    var image: String {
+        Image(imageName)
+    }
+    
+    
+    
+    
+    //coordinates is of type Coordinates
+    private var coordinates: Coordinates
+    
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+    
+    //Nested Coordinates struct that represents the JSON data
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
     
 }
